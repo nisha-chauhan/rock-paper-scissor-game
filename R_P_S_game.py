b003c94cc6ece11ac3037f7f2aab66cd0af37076
Nisha <nishachauhan0318@gmail.com>
2022-05-11 12:19:32 +0530

    second commit

--- R_P_S_game.py
+++ R_P_S_game.py
@@ -26,31 +26,28 @@ scissor=("""
 ---.__(___)
 """)
 
-def play():
-    Game_Images=[rock,paper,scissor]
-    user_choice=int(input("What is yoyur choice! 0 for Rock,1 for Paper,2 for Scissor\n"))
-    if (user_choice>=3 or user_choice<0):
-        print("you choose a invalid no. So You Loose!")
-    else:
-        print(Game_Images[user_choice])
-        
-        computer_choice=random.randint(0,2)
-        print("computer choose:")
-        print(Game_Images[computer_choice])
+Game_Images=[rock,paper,scissor]
+user_choice=int(input("What is yoyur choice! 0 for Rock,1 for Paper,2 for Scissor\n"))
+if (user_choice>=3 or user_choice<0):
+    print("you choose a invalid no. So You Loose!")
+else:
+    print(Game_Images[user_choice])
+    
+    computer_choice=random.randint(0,2)
+    print("computer choose:")
+    print(Game_Images[computer_choice])
 
-        if computer_choice==user_choice:
-            print("it's a draw or tya!")
+    if computer_choice==user_choice:
+        print("it's a draw or tya!")
+    
+    elif (computer_choice==0 and user_choice==2):
+        print("You Loose!")
+    elif(computer_choice==2 and user_choice==1):
+        print("You Loose!")
+    elif(computer_choice>user_choice) :
+        print("You Win!")       
+    elif(computer_choice<user_choice):
+        print("You Win!")
+    else:
+        print("wrong entry")    
         
-        elif (computer_choice==0 and user_choice==2):
-            print("You Loose!")
-        elif(computer_choice==2 and user_choice==1):
-            print("You Loose!")
-        elif(computer_choice>user_choice) :
-            print("You Win!")       
-        elif(computer_choice<user_choice):
-            print("You Win!")
-        else:
-            print("wrong entry")    
-        
-play()
- 
